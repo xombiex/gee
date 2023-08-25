6b7cccb50d3953be61dadfa0e077023e0a7757cc
xombiex <111113958+xombiex@users.noreply.github.com>
2023-08-24 22:06:43 -0400

    Delete dezi_files directory

--- dezi_files/typed.js
+++
@@ -1,7 +0,0 @@
-new Typed('#typed',{
-  strings : [,'#1 in com','ur poor :c','connecting :O','Lawin#9009 *_^','i will behead you c:','why you lagging?','https://swatted.net :D'],
-  typeSpeed : 35,
-  delaySpeed : 90,
-  showCursor: false,
-  loop : true
-})
