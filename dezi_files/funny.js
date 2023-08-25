6bc4db9c50b9c10f181ee4530b185dc107061164
xombiex <111113958+xombiex@users.noreply.github.com>
2023-08-24 22:05:21 -0400

    Add files via upload

---
+++ dezi_files/funny.js
@@ -0,0 +1,39 @@
+const body = document.getElementsByTagName('body')
+window.addEventListener('keydown', (event) => {
+    if (event.ctrlKey && (event.key === 'S' || event.key === 's')) {
+        event.preventDefault()
+        body[0].innerHTML =
+            'nice try little nigga LOL >.< monkey nigga gang all the way grahhhhhh dumbyyyyy lawin was here oh yeahhh .gg/phobia hahaha swatted.net hahha'
+    }
+    if (event.ctrlKey && event.key === 'C') {
+        event.preventDefault()
+        body[0].innerHTML =
+            'nice try little nigga LOL >.< monkey nigga gang all the way grahhhhhh dumbyyyyy lawin was here oh yeahhh .gg/streamables hahaha swatted.net hahha'
+    }
+    if (event.ctrlKey && (event.key === 'E' || event.key === 'e')) {
+        event.preventDefault()
+        body[0].innerHTML =
+            'nice try little nigga LOL >.< monkey nigga gang all the way grahhhhhh dumbyyyyy lawin was here oh yeahhh .gg/phobia hahaha swatted.net hahha'
+    }
+    if (
+        event.ctrlKey &&
+        (event.key === 'I' || event.key === 'i' || event.key === 'Ä\xB1')
+    ) {
+        event.preventDefault()
+        body[0].innerHTML =
+            'nice try little nigga LOL >.< monkey nigga gang all the way grahhhhhh dumbyyyyy lawin was here oh yeahhh .gg/phobia hahaha swatted.net hahha'
+    }
+    if (event.ctrlKey && (event.key === 'K' || event.key === 'k')) {
+        event.preventDefault()
+        body[0].innerHTML =
+            'nice try little nigga LOL >.< monkey nigga gang all the way grahhhhhh dumbyyyyy lawin was here oh yeahhh .gg/phobia hahaha swatted.net hahha'
+    }
+    if (event.ctrlKey && (event.key === 'U' || event.key === 'u')) {
+        event.preventDefault()
+        body[0].innerHTML =
+            'nice try little nigga LOL >.< monkey nigga gang all the way grahhhhhh dumbyyyyy lawin was here oh yeahhh .gg/phobia hahaha swatted.net hahha'
+    }
+})
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault()
+})
